91aba3a5062faa1d19611c67f420cdeabdecd5a0
Daniel Peterson <danrpts@gmail.com>
2021-01-02 14:28:34 -0800

    improved sound list ui and other changes

--- commands/soundboard/list-sounds.js
+++ commands/soundboard/list-sounds.js
@@ -1,32 +1,32 @@
+const colormap = require("colormap");
+const { chain, mapValues, sortBy, clamp } = require("lodash");
 const { MessageEmbed } = require("discord.js");
 const { Command } = require("discord.js-commando");
 
-function alphabetize(arr, key) {
-  return arr.sort((a, b) => {
-    if (a[key] < b[key]) {
-      return -1;
-    } else if (a[key] > b[key]) {
-      return 1;
-    } else {
-      return 0;
-    }
-  });
-}
+const MAX_PAGE_COUNT = 12;
+const COLOR_MAP = colormap({
+  colormap: "portland",
+  nshades: 9,
+  format: "hex",
+  alpha: 1
+});
 
-function asAlphabeticalFields(sounds) {
-  let arr = [];
-  for (const sound in sounds) {
-    const { url, volume } = sounds[sound];
-    arr = [
-      ...arr,
-      {
-        name: `${sound} @ ${volume}%`,
-        value: url,
-        inline: true
-      }
-    ];
-  }
-  return alphabetize(arr, "name");
+function pagedEmbeds(sounds) {
+  return chain(sounds)
+    .mapValues(({ url, volume }, sound) => ({
+      name: `${sound} (${volume}%)`,
+      value: `[:link:](${url})`,
+      inline: true
+    }))
+    .sortBy(["name"])
+    .chunk(MAX_PAGE_COUNT)
+    .map((fields, index, chunks) =>
+      new MessageEmbed()
+        .setColor(COLOR_MAP[index % COLOR_MAP.length])
+        .setTitle(`Page ${index + 1} / ${chunks.length}`)
+        .addFields(fields)
+    )
+    .value();
 }
 
 class ListSoundsCommand extends Command {
@@ -43,68 +43,39 @@ class ListSoundsCommand extends Command {
 
   async run(msg, args) {
     const guildId = msg.guild.id;
-
     const sounds = await this.client.provider.get(guildId, "sounds", {});
-    const embedFields = asAlphabeticalFields(sounds);
+    const pages = pagedEmbeds(sounds);
 
-    let currentIndex = 0;
+    const listingMsg = await msg.channel.send({ embed: pages[0] });
+    await Promise.all(["⏪", "◀️", "▶️", "⏩"].map(c => listingMsg.react(c)));
 
-    let embedSlice = embedFields.slice(currentIndex, currentIndex + 9);
-    const temp = await msg.channel.send({
-      content: "",
-      embed: new MessageEmbed()
-        .setColor("#0099ff")
-        .setTitle("Sounds")
-        .addFields(embedFields.slice(currentIndex, currentIndex + 9))
-        .setFooter(
-          `${currentIndex}-${currentIndex + embedSlice.length} of ${
-            embedFields.length
-          }`
-        )
-    });
-
-    await temp.react("➡️");
-
-    const collector = temp.createReactionCollector(
-      (reaction, user) =>
-        ["⬅️", "➡️"].includes(reaction.emoji.name) &&
-        user.id !== this.client.user.id,
+    const collector = listingMsg.createReactionCollector(
+      (reaction, user) => !user.bot,
       { time: 60000 }
     );
 
-    collector.on("collect", async reaction => {
-      await temp.reactions.removeAll();
-
-      if (reaction.emoji.name === "⬅️") {
-        currentIndex -= 9;
+    let currentIndex = 0;
+    collector.on("collect", async (reaction, user) => {
+      switch (reaction.emoji.name) {
+        case "⏪":
+          currentIndex = 0;
+        case "◀️":
+          currentIndex -= 1;
+          break;
+        case "▶️":
+          currentIndex += 1;
+          break;
+        case "⏩":
+          currentIndex = pages.length - 1;
+          break;
       }
+      currentIndex = clamp(currentIndex, 0, pages.length - 1);
+      await reaction.users.remove(user);
+      await listingMsg.edit({ embed: pages[currentIndex] });
+    });
 
-      if (reaction.emoji.name === "➡️") {
-        currentIndex += 9;
-      }
-
-      embedSlice = embedFields.slice(currentIndex, currentIndex + 9);
-
-      temp.edit({
-        content: "",
-        embed: new MessageEmbed()
-          .setColor("#0099ff")
-          .setTitle("Sounds")
-          .addFields(embedSlice)
-          .setFooter(
-            `${currentIndex}-${currentIndex + embedSlice.length} of ${
-              embedFields.length
-            }`
-          )
-      });
-
-      if (currentIndex > 0) {
-        await temp.react("⬅️");
-      }
-
-      if (currentIndex + 9 < embedFields.length) {
-        await temp.react("➡️");
-      }
+    collector.on("end", async () => {
+      await listingMsg.reactions.removeAll();
     });
   }
 }
