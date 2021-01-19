9ea9f7e96d47d093062ad61b337bffb14429024f
Daniel Peterson <danrpts@gmail.com>
2021-01-18 16:02:11 -0800

    add redlock to pick streamer

--- bot/src/player.js
+++ bot/src/player.js
@@ -1,5 +1,5 @@
 const Redis = require("ioredis");
-const { inspect } = require("util");
+const Redlock = require("redlock");
 
 class Player {
   constructor(msg, guildId) {
@@ -8,62 +8,75 @@ class Player {
     this.client = new Redis({
       host: process.env.REDIS_HOST
     });
+    this.redlock = new Redlock([this.client]);
+    this.listKey = `${this.guildId}:list`;
+    this.lockKey + `${this.guildId}:lock`;
   }
 
   async play({ url, name, volume }) {
     const length = await this.client.rpush(
-      this.guildId,
+      this.listKey,
       JSON.stringify({ url, name, volume })
     );
 
     await this.msg.react("👍");
 
-    // todo: use an expiring redlock
-    if (length === 1) {
-      try {
-        const subscriber = new Redis({
-          host: process.env.REDIS_HOST
+    try {
+      const lock = await this.redlock.lock(this.lockKey, 5000);
+
+      const subscriber = new Redis({
+        host: process.env.REDIS_HOST
+      });
+      subscriber.subscribe(this.guildId);
+      subscriber.on("message", async (channel, message) => {
+        console.info("[STREAMER] received:", message);
+        switch (message) {
+          case "skip":
+            this.dispatcher.end();
+            break;
+          case "clear":
+            this.dispatcher.end();
+            await this.client.del(this.listKey);
+            await lock.unlock();
+            break;
+        }
+      });
+
+      console.info("[STREAMER] up:", this.guildId);
+      const connection = await this.msg.member.voice.channel.join();
+      await connection.voice.setSelfDeaf(true);
+
+      while ((await this.client.llen(this.listKey)) > 0) {
+        const sound = JSON.parse(await this.client.lindex(this.listKey, 0));
+
+        console.info("[STREAMER] processing:", this.guildId, sound);
+
+        this.dispatcher = connection.play(sound.url, {
+          volume: sound.volume,
+          quality: "highestaudio"
         });
-        subscriber.subscribe(this.guildId);
-        subscriber.on("message", (channel, message) => {
-          switch (message) {
-            case "skip":
-              console.info("[STREAMER] received:", message);
-              if (this.dispatcher) {
-                this.dispatcher.end();
-              }
-              break;
+
+        this.dispatcher.on("drain", async () => {
+          try {
+            // todo: extend lock with dynamic value
+            await lock.extend(lock.expiration - Date.now() + 50);
+          } catch (err) {
+            console.info("[STREAMER] extend lock error:", this.guildId, err);
           }
         });
 
-        console.info("[STREAMER] up:", this.guildId);
-        const connection = await this.msg.member.voice.channel.join();
-        await connection.voice.setSelfDeaf(true);
+        await new Promise((resolve, reject) => {
+          this.dispatcher.on("finish", () => resolve());
+          this.dispatcher.on("error", () => reject());
+        });
 
-        while ((await this.client.llen(this.guildId)) > 0) {
-          const sound = JSON.parse(await this.client.lindex(this.guildId, 0));
-
-          console.info("[STREAMER] processing:", this.guildId, sound);
-
-          this.dispatcher = connection.play(sound.url, {
-            volume: sound.volume,
-            quality: "highestaudio"
-          });
-
-          await new Promise((resolve, reject) => {
-            this.dispatcher.on("finish", () => resolve());
-            this.dispatcher.on("error", () => reject());
-          });
-
-          await this.client.lpop(this.guildId);
-        }
-
-        console.info("[STREAMER] down:", this.guildId);
-      } catch (err) {
-        // streamer errored with list full, must unblock next stream
-        await this.client.del(this.guildId);
-        console.info("[STREAMER] errored:", this.guildId, err);
+        await this.client.lpop(this.listKey);
       }
+
+      await lock.unlock();
+      console.info("[STREAMER] down:", this.guildId);
+    } catch (err) {
+      // todo standby
     }
   }
 
@@ -73,13 +86,12 @@ class Player {
   }
 
   async clear() {
-    await this.client.del(this.guildId);
-    await this.client.publish(this.guildId, "skip");
+    await this.client.publish(this.guildId, "clear");
     await this.msg.react("👍");
   }
 
   async list() {
-    const list = await this.client.lrange(this.guildId, 0, -1);
+    const list = await this.client.lrange(this.listKey, 0, -1);
     await this.msg.reply(
       JSON.stringify(list.map(payload => JSON.parse(payload).name))
     );
